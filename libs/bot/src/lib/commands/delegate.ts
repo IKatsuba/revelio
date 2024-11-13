0faf04e61d47fabdbeaae9fd7750a8f3f2d894da
Igor Katsuba <igor@katsuba.dev>
2024-11-13 23:44:35 +0200

    initial

--- libs/bot/src/lib/commands/delegate.ts
+++
@@ -1,7 +0,0 @@
-import { BotContext } from '@revelio/bot-utils';
-
-export async function delegate(ctx: BotContext) {
-  await ctx.replyWithChatAction('typing');
-
-  await ctx.reply(`I'm sorry, I don't understand that command. Please try again.`);
-}
