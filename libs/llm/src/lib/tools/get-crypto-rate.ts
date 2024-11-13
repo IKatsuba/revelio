6eec49f6ef914fe3f5529560bf9bc26f9e701e61
Igor Katsuba <igor@katsuba.dev>
2024-11-13 22:25:40 +0200

    initial

--- libs/llm/src/lib/tools/get-crypto-rate.ts
+++ libs/llm/src/lib/tools/get-crypto-rate.ts
@@ -7,9 +7,16 @@ export const getCryptoRate = tool({
     currency: z.string().describe('the currency code'),
   }),
   execute: async ({ currency }, { abortSignal }) => {
-    const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`, {
-      signal: abortSignal,
-    });
+    const response = await fetch(`https://api.cryptomus.com/v1/exchange-rate/${currency}/list`);
+
     return response.json();
   },
 });
+
+async function md5(input: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('MD5', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
